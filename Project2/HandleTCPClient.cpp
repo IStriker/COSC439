d8a57e0fa02f921087d000d354842182593c074d
Abdul Aldhaheri <abdulaldhaheri@Abduls-MacBook-Pro.local>
2017-04-09 10:19:06 -0400

    recieve works

--- Project2/HandleTCPClient.cpp
+++ Project2/HandleTCPClient.cpp
@@ -2,52 +2,70 @@
 #include <sys/socket.h> /* for recv() and send() */
 #include <unistd.h>     /* for close() */
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
 
-#define RCVBUFSIZE 23   /* Size of receive buffer */
+#define RCVBUFSIZE 30   /* Size of send Buffer */
 
 using namespace std;
 
 void DieWithError(char *errorMessage);  /* Error handling function */
+string read_news_file();
 
 void HandleTCPClient(int clntSocket)
 {
     char echoBuffer[RCVBUFSIZE];        /* Buffer for echo string */
     int recvMsgSize;                    /* Size of received message */
-    char *message;
+    //char *message;
+    string str;
     int index = 0, i;
 
     /* Receive message from client */
     if ((recvMsgSize = recv(clntSocket, echoBuffer, RCVBUFSIZE, 0)) < 0)
         DieWithError("recv() failed");
 
-    cout << "before assignment" << endl;
+    echoBuffer[recvMsgSize] = '\0';
+
     /* check what is the request */
     if(strcmp(echoBuffer, "news feed") == 0) {
-        cout << "inside true" << endl;
-        message = "Hello this ahmed aldhaheri and I am testing my TCP program if it works\0";
+        str = read_news_file();
+    } else if(){
+
     }
+
+    /* assign new content string to char array and get size*/
+    char message[str.length()];
+    strcpy(message, str.c_str());
     int size = strlen(message);
-    cout << "after assignment" << endl;
-    cout << "size: " << size << endl;
-    cout << "message: " << message << endl;
-    /** @test */
-    /* Send received string and receive again until end of transmission */
+
+    /* Send news string until end of transmission */
     while (index <= size)      /* zero indicates end of transmission */
     {
-
+        memset(echoBuffer, '\0', sizeof(char) * RCVBUFSIZE);
+        //cout << "after memset: " << echoBuffer << endl;
+        for(i = 0; i < RCVBUFSIZE; i++){
+            echoBuffer[i] = '\0';
+        }
         for(i = 0; i < RCVBUFSIZE; i++){
             if((i + index) < size) {
                 echoBuffer[i] = message[i + index];
-                cout << "i + index: " << (i + index) << endl;
+                //cout << "i + index: " << (i + index) << endl;
             }
             else{
-                for(i = i; i < RCVBUFSIZE; i++)
+                /*for(i = i; i < RCVBUFSIZE; i++) {
                     echoBuffer[i] = '\0';
+                }*/
+                cout << "inside else" << endl;
+                echoBuffer[i] = '\0';
+                break;
+
             }
 
         }
         index += RCVBUFSIZE;
         cout << echoBuffer << endl;
+        recvMsgSize = strlen(echoBuffer);
         /* Echo message back to client */
         if (send(clntSocket, echoBuffer, recvMsgSize, 0) != recvMsgSize) {
             //DieWithError("send() failed");
@@ -60,3 +78,31 @@ void HandleTCPClient(int clntSocket)
 
     close(clntSocket);    /* Close client socket */
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * this function responds to the recieve today's
+ * news request by read the day news file and return
+ * the file content in a string
+ *
+ * @return string
+ */
+string read_news_file(){
+
+    /* open file to read */
+    string file_name = "11-21-2016.txt";
+    ifstream file(file_name.c_str());
+    stringstream buffer;
+
+    buffer << file.rdbuf();
+    string str = buffer.str();
+    return str;
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * this function searches for an item tag
+ */
--- Project2/TCPClient.cpp
+++ Project2/TCPClient.cpp
@@ -5,9 +5,13 @@
 #include <string.h>     /* for memset() */
 #include <unistd.h>     /* for close() */
 #include <iostream>     /* for cout, cin*/
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <vector>
 
 
-#define RCVBUFSIZE 32   /* Size of receive buffer */
+#define RCVBUFSIZE 30    /* Size of receive buffer */
 
 using namespace std;
 
@@ -15,6 +19,9 @@ using namespace std;
 void DieWithError(char *errorMessage);  /* Error handling function */
 string menu();        /* Display menu for user */
 string search_keyword();
+void display(string);
+
+int screen_width = 0;           /* terminal width is 80 columns (0-79) */
 
 int main(int argc, char *argv[])
 {
@@ -22,13 +29,14 @@ int main(int argc, char *argv[])
     struct sockaddr_in echoServAddr; /* Echo server address */
     unsigned short echoServPort;     /* Echo server port */
     char *servIP;                    /* Server IP address (dotted quad) */
-    char *echoString;                /* String to send to echo server */
+    //echoString;                /* String to send to echo server */
     char echoBuffer[RCVBUFSIZE];     /* Buffer for echo string */
     unsigned int echoStringLen;      /* Length of string to echo */
     int bytesRcvd, totalBytesRcvd;   /* Bytes read in single recv() 
                                         and total bytes read */
+    string full_message = "";
 
-    if ((argc < 3) || (argc > 4))    /* Test for correct number of arguments */
+    if ((argc < 2) || (argc > 3))    /* Test for correct number of arguments */
     {
        fprintf(stderr, "Usage: %s <Server IP> <Echo Word> [<Echo Port>]\n",
                argv[0]);
@@ -36,10 +44,10 @@ int main(int argc, char *argv[])
     }
 
     servIP = argv[1];             /* First arg: server IP address (dotted quad) */
-    echoString = argv[2];         /* Second arg: string to echo */
+    //echoString = argv[2];         /* Second arg: string to echo */
 
-    if (argc == 4)
-        echoServPort = atoi(argv[3]); /* Use given port, if any */
+    if (argc == 3)
+        echoServPort = atoi(argv[2]); /* Use given port, if any */
     else
         echoServPort = 7;  /* 7 is the well-known port for the echo service */
 
@@ -53,32 +61,37 @@ int main(int argc, char *argv[])
     echoServAddr.sin_addr.s_addr = inet_addr(servIP);   /* Server IP address */
     echoServAddr.sin_port        = htons(echoServPort); /* Server port */
 
-    /* Establish the connection to the echo server */
-    if (connect(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
-        DieWithError("connect() failed");
+    while(true){
 
-    echoStringLen = strlen(echoString);          /* Determine input length */
-    string message = menu();
-    strcpy(echoString, message.c_str());
+        /* Establish the connection to the echo server */
+        if (connect(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+            DieWithError("connect() failed");
 
-    /* Send the string to the server */
-    if (send(sock, echoString, echoStringLen, 0) != echoStringLen)
-        DieWithError("send() sent a different number of bytes than expected");
+        /* get users choice from menu */
+        string message = menu();
+        cout << "message: " << message << endl;
 
-    /* Receive the same string back from the server */
-    totalBytesRcvd = 0;
-    cout << "Received: ";                /* Setup to print the echoed string */
-    while (true)//totalBytesRcvd < echoStringLen)
-    {
-        /* Receive up to the buffer size (minus 1 to leave space for
-           a null terminator) bytes from the sender */
-        if ((bytesRcvd = recv(sock, echoBuffer, RCVBUFSIZE - 1, 0)) <= 0) {
-            //DieWithError("recv() failed or connection closed prematurely");
-            break;
+        /* Send the string to the server */
+        if (send(sock, message.c_str(), strlen(message.c_str()), 0) != strlen(message.c_str()))
+            DieWithError("send() sent a different number of bytes than expected");
+
+        /* Receive the same string back from the server */
+        totalBytesRcvd = 0;
+        cout << "Received: " << endl;                /* Setup to print the echoed string */
+        while (true) {
+            memset(echoBuffer, '\0', RCVBUFSIZE);
+            /* Receive up to the buffer size (minus 1 to leave space for
+               a null terminator) bytes from the sender */
+            if ((bytesRcvd = recv(sock, echoBuffer, RCVBUFSIZE - 1, 0)) <= 0) {
+                //DieWithError("recv() failed or connection closed prematurely");
+                break;
+            }
+            totalBytesRcvd += bytesRcvd;            /* Keep tally of total bytes */
+            //cout << "bytes recieved: " << bytesRcvd << endl;
+            echoBuffer[bytesRcvd] = '\0';           /* Terminate the string! */
+            cout << echoBuffer;             /* Print the echo buffer */
+
         }
-        totalBytesRcvd += bytesRcvd;            /* Keep tally of total bytes */
-        echoBuffer[bytesRcvd] = '\0';           /* Terminate the string! */
-        cout << echoBuffer;             /* Print the echo buffer */
     }
 
     cout << "\n";    /* Print a final linefeed */
@@ -139,4 +152,28 @@ string menu(){
 string search_keyword(){
 
     return "";
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * takes string message and displays in a readable format
+ * @param message
+ */
+void display(string message){
+    //cout << "string: " << message << endl;
+    stringstream str(message);
+    istream_iterator<string> begin(str);
+    istream_iterator<string> end;
+    vector<string> vect_strings (begin, end);
+    ostringstream buffer;
+    copy(vect_strings.begin(), vect_strings.end(), ostream_iterator<string>(buffer,""));
+
+    for(int i = 0; i < vect_strings.size(); i++){
+
+        cout << vect_strings[i] << endl;
+    }
+
+
 }
